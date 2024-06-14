5bfdae551e00c8b6a7a409e4f69c53d83923549c
seungsuuu <tmdtn013@naver.com>
2024-06-14 19:30:43 +0900

    [feat] update Test [refactor] setup
    - User 의 정보를 변경하는 update() 테스트
    - 중복되는 코드를 @BeforeEach 가 적용된 setup() 으로 이동

--- src/test/java/com/sparta/newsfeedteamproject/entity/UserTest.java
+++ src/test/java/com/sparta/newsfeedteamproject/entity/UserTest.java
@@ -4,30 +4,32 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
     User user;
+    String username;
+    String password;
+    String name;
+    String email;
+    String userInfo;
+    Status status;
+    LocalDateTime statusModTime;
 
     @BeforeEach
     void setUp() {
         user = new User();
-    }
 
-    @Test
-    @DisplayName("User Entity 생성자 Test")
-    void constructorUser() {
-
-        // given
-        String username = "tmdtn01345";
-        String password = "@Tmdtn01345";
-        String name = "김승수";
-        String email = "snol4331@gmail.com";
-        String userInfo = "안녕하세요";
-        Status status = Status.UNAUTHORIZED;
-        LocalDateTime statusModTime = LocalDateTime.now();
+        username = "tmdtn01345";
+        password = "@Tmdtn01345";
+        name = "김승수";
+        email = "snol4331@gmail.com";
+        userInfo = "안녕하세요";
+        status = Status.UNAUTHORIZED;
+        statusModTime = LocalDateTime.now();
 
         user.setUsername(username);
         user.setPassword(password);
@@ -36,6 +38,13 @@ class UserTest {
         user.setUserInfo(userInfo);
         user.setStatus(status);
         user.setStatusModTime(statusModTime);
+    }
+
+    @Test
+    @DisplayName("User Entity 생성자 Test")
+    void constructorUser() {
+
+        // given
 
         // when
         User constructorUser = new User(username, password, name, email, userInfo, status, statusModTime);
@@ -55,22 +64,7 @@ class UserTest {
     void deleteRefreshToken() {
 
         // given
-        String username = "tmdtn01345";
-        String password = "@Tmdtn01345";
-        String name = "김승수";
-        String email = "snol4331@gmail.com";
-        String userInfo = "안녕하세요";
-        Status status = Status.UNAUTHORIZED;
-        LocalDateTime statusModTime = LocalDateTime.now();
         String refreshToken = "asdf1234";
-
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setName(name);
-        user.setEmail(email);
-        user.setUserInfo(userInfo);
-        user.setStatus(status);
-        user.setStatusModTime(statusModTime);
         user.setRefreshToken(refreshToken);
 
         // when
@@ -85,7 +79,20 @@ class UserTest {
     @Test
     @DisplayName("User Entity update() Test")
     void update() {
+        // given
+        String updateName = "김슨수";
+        String updateUserInfo = "안녕하세요. 수정입니다.";
+        String updatePassword = "@Tmdtn0134567";
+        LocalDateTime updateModifiedAt = LocalDateTime.now();
 
+        // when
+        user.update(updateName, updateUserInfo, updatePassword, updateModifiedAt);
+
+        // then
+        assertEquals(updateName, user.getName());
+        assertEquals(updateUserInfo, user.getUserInfo());
+        assertEquals(updatePassword, user.getPassword());
+        assertEquals(updateModifiedAt, user.getModifiedAt());
     }
 
     @Test
