0868240f2a1621bcbb1c73c078ad6cd2f819c1af
seungsuuu <tmdtn013@naver.com>
2024-06-16 20:59:54 +0900

    [feat] CommentService 통합테스트 중 createComment 테스트
    - CommentReqDto 를 생성해서 사용하기 위해 @AllArgsConstructor 추가
    - createCommentTest() 를 통해 createComment 테스트

--- src/main/java/com/sparta/newsfeedteamproject/dto/comment/CommentReqDto.java
+++ src/main/java/com/sparta/newsfeedteamproject/dto/comment/CommentReqDto.java
@@ -1,9 +1,11 @@
 package com.sparta.newsfeedteamproject.dto.comment;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CommentReqDto {
     @NotBlank(message = "[contents:blank] 댓글 내용을 작성해주세요!")
     private String contents;
---
+++ src/test/java/com/sparta/newsfeedteamproject/service/CommentServiceIntegrationTest.java
@@ -0,0 +1,58 @@
+package com.sparta.newsfeedteamproject.service;
+
+import com.sparta.newsfeedteamproject.dto.MessageResDto;
+import com.sparta.newsfeedteamproject.dto.comment.CommentReqDto;
+import com.sparta.newsfeedteamproject.dto.comment.CommentResDto;
+import com.sparta.newsfeedteamproject.entity.Feed;
+import com.sparta.newsfeedteamproject.entity.User;
+import com.sparta.newsfeedteamproject.repository.UserRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CommentServiceIntegrationTest {
+
+    @Autowired
+    CommentService commentService;
+
+    @Autowired
+    FeedService feedService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    User user;
+    Feed feed;
+    CommentResDto createComment = null;
+    String updateContents = "";
+
+
+    @Test
+    @Order(1)
+    @DisplayName("댓글 생성(createComment) 테스트")
+    void createCommentTest() {
+
+        // given
+        String contents = "댓글 작성입니다.";
+        CommentReqDto requestDto = new CommentReqDto(contents);
+        user = userRepository.findById(1L).orElse(null);
+        feed = feedService.findFeed(1L);
+
+        // when
+        MessageResDto<CommentResDto> messageComment = commentService.createComment(feed.getId(), requestDto, user);
+        CommentResDto comment = messageComment.getData();
+
+        // then
+        assertNotNull(comment.getId());
+        assertEquals(contents, comment.getContents());
+        assertEquals(user.getUsername(), comment.getUsername());
+        assertEquals(feed.getId(), comment.getFeedId());
+        createComment = comment;
+    }
+}
