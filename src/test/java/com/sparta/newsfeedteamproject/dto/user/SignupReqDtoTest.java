580c1ccba9d5f930afe700c53c2e992bce2b8dea
seungsuuu <tmdtn013@naver.com>
2024-06-16 17:04:49 +0900

    [feat] SignupReqDto 여러 개의 validation 실패 테스트
    - SignupReqDto 에서 여러 필드의 validation 실패 테스트

--- src/test/java/com/sparta/newsfeedteamproject/dto/user/SignupReqDtoTest.java
+++ src/test/java/com/sparta/newsfeedteamproject/dto/user/SignupReqDtoTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -131,5 +133,33 @@ class SignupReqDtoTest {
         for (ConstraintViolation<SignupReqDto> violation : violations) {
             assertEquals("[email:pattern] 이메일 형식을 맞춰주세요!",violation.getMessage());
         }
+    }
+
+    @Test
+    @DisplayName("SignupReqDto 여러 개의 Validation 테스트: 실패")
+    void validationsFail() {
+
+        // given
+        SignupReqDto reqDto = new SignupReqDto(
+                "tmdtn01345",
+                "@tmdtn01345",   // 대문자 누락
+                "",   // 입력 X
+                "snol4331gmail.com",   // 이메일 형식 X
+                "안녕하세요1"
+        );
+        List<String> messageList = new ArrayList<>();
+
+        // when
+        Set<ConstraintViolation<SignupReqDto>> violations = validator.validate(reqDto);
+
+        // then
+        for (ConstraintViolation<SignupReqDto> violation : violations) {
+            messageList.add(violation.getMessage());
+        }
+
+        assertTrue(messageList.contains("[password:pattern] 숫자, 영문 대소문자, 특수기호를 최소 한개씩 포함하여 작성해주세요!"));
+        assertTrue(messageList.contains("[name:blank] 사용자 이름을 작성해주세요!"));
+        assertTrue(messageList.contains("[email:pattern] 이메일 형식을 맞춰주세요!"));
+        assertFalse(messageList.contains("[username:size] 10자 이상 20자 이하로 작성해주세요!"));
     }
 }
