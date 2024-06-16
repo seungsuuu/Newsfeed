3d54f595fe7e16be0329a2683e4e72b0bdf86f50
seungsuuu <tmdtn013@naver.com>
2024-06-16 16:40:33 +0900

    [feat] SignupReqDto validation 성공 테스트
    - 테스트에서 생성자를 사용하기 위해 SignupReqDto 클래스에 @AllArgsConstructor 추가
    - SignupReqDto 의 모든 validation 을 성공하는 테스트

--- src/test/java/com/sparta/newsfeedteamproject/dto/user/SignupReqDtoTest.java
+++ src/test/java/com/sparta/newsfeedteamproject/dto/user/SignupReqDtoTest.java
@@ -1,7 +1,47 @@
 package com.sparta.newsfeedteamproject.dto.user;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SignupReqDtoTest {
 
+    private static ValidatorFactory factory;
+    private static Validator validator;
+
+    @BeforeAll
+    static void setup() {
+        factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    @DisplayName("SignupReqDto Validation 테스트: 성공")
+    void allValidSuccess() {
+
+        // given
+        SignupReqDto reqDto = new SignupReqDto(
+                "tmdtn01345",
+                "@Tmdtn01345",
+                "김승수",
+                "snol4331@gmail.com",
+                "안녕하세요1"
+        );
+
+        // when
+        Set<ConstraintViolation<SignupReqDto>> violations = validator.validate(reqDto);
+
+        // then
+        for (ConstraintViolation<SignupReqDto> violation : violations) {
+            assertNull(violation);
+        }
+    }
 }
