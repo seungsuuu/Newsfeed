96c4b31874e9a4a227673906dc931ca53e489bb2
seungsuuu <tmdtn013@naver.com>
2024-06-17 11:32:38 +0900

    [init] CommentController Test setup data

--- src/test/java/com/sparta/newsfeedteamproject/controller/CommentMvcTest.java
+++ src/test/java/com/sparta/newsfeedteamproject/controller/CommentMvcTest.java
@@ -1,5 +1,66 @@
 package com.sparta.newsfeedteamproject.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.newsfeedteamproject.config.SecurityConfig;
+import com.sparta.newsfeedteamproject.entity.Status;
+import com.sparta.newsfeedteamproject.entity.User;
+import com.sparta.newsfeedteamproject.security.UserDetailsImpl;
+import com.sparta.newsfeedteamproject.service.CommentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.security.Principal;
+import java.time.LocalDateTime;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@WebMvcTest(
+        controllers = {CommentController.class},
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        classes = SecurityConfig.class
+                )
+        }
+)
 class CommentMvcTest {
+
+    private MockMvc mvc;
+    private Principal mockPrincipal;
+    @Autowired
+    private WebApplicationContext context;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    CommentService commentService;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity(new MockSpringSecurityFilter()))
+                .build();
+    }
+
+    private void mockUserSetup() {
+
+        String username = "tmdtn01345";
+        String password = "@Tmdtn01345";
+        String email = "snol4331@gmail.com";
+        String name = "김승수";
+        String userInfo = "안녕하세요";
+        Status status = Status.ACTIVATE;
+        LocalDateTime statusModTime = LocalDateTime.now();
+        User testUser = new User(username, password, name, email, userInfo, status, statusModTime);
+        UserDetailsImpl testUserDetails = new UserDetailsImpl(testUser);
+        mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, "", testUserDetails.getAuthorities());
+    }
 
 }
